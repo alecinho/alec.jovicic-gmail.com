09753360a1eaa8b3375aaa449b01055cc7aab60e
alecinho <57950811+alecinho@users.noreply.github.com>
2020-03-30 13:17:16 +0200

    yes
    +++

---
+++ test.py
@@ -0,0 +1,21 @@
+import sqlite3 as lite
+from sqlite3 import Error
+import tkinter as tk
+from glob import glob
+ 
+def create(obj):
+	db = obj.e.get()
+ 
+	if db[-3] == ".db":
+		pass
+	else:
+		db = db + ".db"
+	try:
+		conn = lite.connect(db)
+		return conn
+	except Error as e:
+		print(e)
+	finally:
+		conn.close()
+		obj.lb.insert(tk.END, db)
+		obj.db.set("")
