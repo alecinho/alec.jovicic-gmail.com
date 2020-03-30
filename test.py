e46bf740cafcf200ea5dc0a49f6a5d5e27dc1b97
alecinho <57950811+alecinho@users.noreply.github.com>
2020-03-30 15:02:59 +0200

    Update test.py
    +++++

--- test.py
+++ test.py
@@ -3,54 +3,107 @@ from sqlite3 import Error
 import tkinter as tk
 from glob import glob
  
-def create(obj):
-	db = obj.e.get()
  
-	if db[-3] == ".db":
-		pass
-	else:
-		db = db + ".db"
-	try:
-		conn = lite.connect(db)
-		return conn
-	except Error as e:
-		print(e)
-	finally:
-		conn.close()
-		obj.lb.insert(tk.END, db)
-		obj.db.set("")
  
-class Window:
-	def __init__(self):
-		self.win = tk.Tk()
-		self.label()
-		self.entry()
-		self.button()
-		self.listbox()
+class App:
+    # window
+    def __init__(self, root):
  
-	def label(self):
-		self.l = tk.Label(self.win, text="Datenbankerstellen [namen eingeben]")
-		self.l.pack()
+        self.fields = []
+        self.root = root
+        self.label()
+        self.entry()
+        self.button()
+        self.listbox()
+        self.db_name_widgets()
+        self.tb_name_widgets()
+        self.fields_widgets()
+        self.btn_create_table()
  
-	def entry(self):
-		self.db = tk.StringVar()
-		self.e = tk.Entry(self.win, textvariable=self.db)
-		self.e.pack()
+    def label(self):
+        self.l = tk.Label(self.root, text="Create a db [insert the name]")
+        self.l.pack()
  
-	def button(self):
-		self.b = tk.Button(self.win, text="Datenbank erstellen", command= lambda: create(self))
-		self.b.pack()
+    def entry(self):
+        self.db = tk.StringVar()
+        self.e = tk.Entry(self.root, textvariable=self.db)
+        self.e.pack()
  
-	def listbox(self):
-		self.lb = tk.Listbox(self.win)
-		self.lb.pack()
-		self.show_db()
+    def button(self):
+        self.b = tk.Button(self.root, text="Create DB", command= lambda: self.mk_db())
+        self.b.pack()
  
-	def show_db(self):
-		for file in glob("*.db"):
-			self.lb.insert(tk.END, file)
+    def listbox(self):
+        self.lb = tk.Listbox(self.root)
+        self.lb.pack()
+        self.show_db()
  
-		
-		self.win.mainloop()
+    def db_name_widgets(self):
+        # label and Entry for Database name
+        self.ldbname = tk.Label(self.root, text="Insert Database name")
+        self.ldbname.pack()
+        self.dbn = tk.StringVar()
+        self.edb = tk.Entry(self.root, textvariable = self.dbn)
+        self.edb.pack()
  
-win = Window()
+    def tb_name_widgets(self):
+        self.ltbname = tk.Label(self.root, text="Insert Table name")
+        self.ltbname.pack()
+        self.tbn = tk.StringVar()
+        self.etb = tk.Entry(self.root, textvariable = self.tbn)
+        self.etb.pack()
+ 
+    def fields_widgets(self):
+        self.lflname = tk.Label(self.root, text="Insert Fields name and type\n followeb by a comma, one by one,\nclicking once for each field.")
+        self.lflname.pack()
+        self.vfl = tk.StringVar()
+        self.efl = tk.Entry(self.root, textvariable = self.vfl)
+        self.efl.pack()
+        self.bfl = tk.Button(self.root, text="Create Field", command= lambda: self.mk_fl())
+        self.bfl.pack()
+ 
+    def btn_create_table(self):
+        self.btb = tk.Button(self.root, text="Create Table", command= lambda: self.mk_tb(self.dbn, self.tbn))
+        self.btb.pack()
+ 
+    def show_db(self):
+        for file in glob("*.db"):
+            self.lb.insert(tk.END, file)
+ 
+    def mk_db(self):
+        db = self.e.get()
+        if db.endswith(".db"):
+            pass
+        else:
+            db = db + ".db"
+        try:
+            conn = lite.connect(db)
+            if db in self.lb.get(0, tk.END):
+                pass
+            else:
+                self.lb.insert(tk.END, db)
+            return conn
+        except Error as e:
+            print(e)
+        finally:
+            self.db.set("")
+            conn.close()
+ 
+    def mk_tb(self, dbn, tbn):
+        self.conn = lite.connect(dbn.get())
+        self.cur = self.conn.cursor()
+        self.fields = "".join(self.fields)
+        self.cur.execute("""create table {} (
+        {});""".format(tbn, self.fields))
+        self.fields = []
+        self.conn.close()
+ 
+    def mk_fl(self):
+        self.fields.append(self.efl.get())
+        self.vfl.set("")
+ 
+ 
+ 
+root = tk.Tk()  
+win = App(root)
+root.mainloop()
